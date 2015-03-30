551b10731bb40b249fcb161a620800be305aba19
aurelien-rainone <aurelien.rainone@gmail.com>
2015-03-30 18:48:03 +0200

    add lastmod.sh
      useful little scripts to know which are the N last modified files
      in a folder

---
+++ lastmod.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+
+# recursively look for the last n (default 1) modified files in a directory
+
+N=${1:-1}
+
+find . -type f -printf '%T@ %p\n' | sort -n | tail "-$N" | cut -f2- -d" "
